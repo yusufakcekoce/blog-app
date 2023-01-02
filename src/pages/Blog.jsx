579e52e0bfcd04d0dcaa4e542d8ef82dcddfe282
Yusuf Akçekoce <yakcekoce@gmail.com>
2023-01-02 17:32:32 +0300

    Delete & edit buttons added

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -40,6 +40,22 @@ function Blog() {
           <div>
             <p>{data.title}</p>
           </div>
+
+          <div>
+            <button
+              type="submit"
+              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+            >
+              Delete
+            </button>
+
+            <button
+              type="submit"
+              className="text-white  bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            >
+              Edit
+            </button>
+          </div>
         </div>
       )}
     </div>
